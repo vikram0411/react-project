ca8e608eaedc00b7d19db2cfdbdb116acd05c669
vikram0411 <vikram987314@gmail.com>
2025-05-05 06:58:08 +0530

    dependencies

--- react-project/app.js
+++ react-project/app.js
@@ -1,9 +1,12 @@
 // const word = React.createElement("H2",{id:"heading"},"header do not touch");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const parent_child =React.createElement("Div" ,{id: "parent"},
-    React.createElement("Div",{id : "Child"} ,
-        [React.createElement("H1",{},"I am a H1 tag"), React.createElement("H2",{},"I am a H1 tag")]
+const parent_child =React.createElement("div" ,{id: "parent"},
+    React.createElement("div",{id : "Child"} ,
+        React.createElement("h1",{},"I am a H1 tag")
     )
 )
 root.render(parent_child);
